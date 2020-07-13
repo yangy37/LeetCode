cdbbbe20fd1e7f0ce384c224161b101308611c29
yangy37 <48518302+yangy37@users.noreply.github.com>
2020-07-12 22:31:13 -0400

    change format

--- Java/836 Rectangle Overlap.java
+++ Java/836 Rectangle Overlap.java
@@ -1,10 +1,12 @@
 class Solution {
     public boolean isRectangleOverlap(int[] a, int[] b) {
-        if (a[0] > b[0])
+        if (a[0] > b[0]) {
             return isRectangleOverlap(b, a);
+        }
         
-        if (a[0] == b[0] && a[1] > b[1])
+        if (a[0] == b[0] && a[1] > b[1]) {
             return isRectangleOverlap(b, a);
+        }
         
         if (a[2] <= b[0]) {
             return false;
