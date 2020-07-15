da1753308c52ef6b77f9a43c4e681d282c7e2921
yangy37 <48518302+yangy37@users.noreply.github.com>
2020-07-15 09:18:02 -0400

    Format change

--- Java/814. Binary Tree Pruning.java
+++ Java/814. Binary Tree Pruning.java
@@ -14,37 +14,33 @@
  * }
  */
 class Solution {
-    public int dfs(TreeNode root)
-{
-    if(root==null)
-    {
-        return 0;
-    }
-    int l=0,r=0;
-    if(root.left!=null)
-    {
-        l=dfs(root.left);
-        if(l==0)
-        {
-            root.left=null;
+    public int dfs(TreeNode root) {
+        if(root==null) {
+            return 0;
         }
-    }
-    if(root.right!=null)
-    {
-        r=dfs(root.right);
-        if(r==0)
-        {
-            root.right=null;
+        int l=0,r=0;
+        if(root.left!=null) {
+            l=dfs(root.left);
+            if(l==0)
+            {
+                root.left=null;
+            }
         }
+        if(root.right!=null) {
+            r=dfs(root.right);
+            if(r==0)
+            {
+                root.right=null;
+            }
+        }
+        return root.val+l+r;
     }
-    return root.val+l+r;
-}
-public TreeNode pruneTree(TreeNode root) {
-    int val=dfs(root);
-    if(val==0)
-    {
-        root=null;
+    
+    public TreeNode pruneTree(TreeNode root) {
+        int val=dfs(root);
+        if(val==0) {
+            root=null;
+        }
+        return root;
     }
-    return root;
-}
 }
